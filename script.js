7e6f849e77dad6c21c79a65a9fc4b4b075000215
Mike Lofgren <mikeylofgren@gmail.com>
2023-01-09 21:18:01 -0800

    set up board

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello")
+document.addEventListener('DOMContentLoaded', () =>  {
+  const grid = document.querySelector('.grid')
+  let width = 10;
+
+  //create Board
+
+  for (let i =0; i < width*width; i ++) {
+    const square = document.createElement('div')
+    square.setAttribute('id', i)
+    grid.appendChild(square)
+  }
+
+})
